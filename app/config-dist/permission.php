684009c368730872ff7aa7cab48ab0a55eca73ef
Marcos David <david.jc7@gmail.com>
2025-02-22 16:03:52 -0300

    fit: config dist

--- app/config-dist/permission.php
+++ app/config-dist/permission.php
@@ -1,10 +1,13 @@
 <?php
 return [
-    'host'  =>  "",
-    'port'  =>  "",
-    'name'  =>  "app/database/permission.db",
-    'user'  =>  "",
-    'pass'  =>  "",
-    'type'  =>  "sqlite",
-    'prep'  =>  "1"
+    'host'  =>  "127.0.0.1",
+    'port'  =>  "3306",
+    'name'  =>  "sysapae",
+    'user'  =>  "root",
+    'pass'  =>  "app@123.",
+    'type'  =>  "mysql",
+    'prep'  =>  "1",
+    'char'  =>  "utf8mb4",
+    'zone'  =>  "America/Sao_Paulo",
+    'slog'  =>  "SystemSqlLogService"
 ];
