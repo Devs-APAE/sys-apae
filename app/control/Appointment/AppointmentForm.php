481d79dbe9cf6be3ad63993133c4b2b795d597b6
tonho <tonysousalopes555@gmail.com>
2025-03-20 13:38:33 -0300

    Alterando AppointmentForm para se adequar as demandas exigidas

--- app/control/Appointment/AppointmentForm.php
+++ app/control/Appointment/AppointmentForm.php
@@ -1,5 +1,7 @@
 <?php
 
+use Adianti\Widget\Wrapper\TDBUniqueSearch;
+
 /**
  * AppointmentForm Form
  * @author Tony Sousa Lopes
@@ -18,60 +20,59 @@ class AppointmentForm extends TPage
 
         // Cria o formulário
         $this->form = new BootstrapFormBuilder('form_Appointment');
-        $this->form->setFormTitle('FORMULÁRIO DE AGENDAMENTO');
+        $this->form->setFormTitle('Atendimento');
         $this->form->setFieldSizes('100%');
 
         // Campos do formulário
         $id = new THidden('id');
 
         $appointment_type_id = new TDBCombo('appointment_type_id', 'app', 'AppointmentType', 'id', 'name');
-        $appointment_type_id->addValidation('Tipo de Agendamento', new TRequiredValidator());
+        $appointment_type_id->addValidation('Tipo de Atendimento', new TRequiredValidator());
 
-        $professional_id = new TDBCombo('professional_id', 'app', 'Professional', 'id', 'name');
+        $professional_id = new TDBUniqueSearch('professional_id', 'app', 'Professional', 'id', 'name');
+        $professional_id->setMinLength(1); // Carrega sugestões após 1 caractere digitado   
+        $professional_id->setMask('{name} ({cpf})');
         $professional_id->addValidation('Profissional', new TRequiredValidator());
-
-        $patient_id = new TDBCombo('patient_id', 'app', 'Patient', 'id', 'name');
+        
+        $patient_id = new TDBUniqueSearch('patient_id', 'app', 'Patient', 'id', 'name');
+        $patient_id->setMinLength(1);
+        $patient_id->setMask('{name} ({cpf})');
         $patient_id->addValidation('Paciente', new TRequiredValidator());
+        
 
         $appointment_date = new TDate('appointment_date');
         $appointment_date->setMask('dd/mm/yyyy');
         $appointment_date->setDatabaseMask('yyyy-mm-dd');
-        $appointment_date->addValidation('Data do Agendamento', new TRequiredValidator());
+        $appointment_date->setValue(date('Y-m-d'));
+        $appointment_date->addValidation('Data do Atendimento', new TRequiredValidator());
 
         $notes = new TText('notes');
-        $notes->placeholder = 'Observações sobre o agendamento';
+        $notes->placeholder = 'Observações sobre o atendimento';
 
         // Adiciona os campos ao formulário
         $this->form->addFields([$id]);
-
+        
         $row = $this->form->addFields(
-            [new TLabel('Tipo de Agendamento *'), $appointment_type_id],
+            [new TLabel('Paciente *'), $patient_id],
+            [new TLabel('Tipo de Atendimento *'), $appointment_type_id]
+        );
+        $row->layout = ['col-sm-8', 'col-sm-4'];
+        
+        $row = $this->form->addFields(
             [new TLabel('Profissional *'), $professional_id],
-            [new TLabel('Paciente *'), $patient_id]
+            [new TLabel('Data do Atendimento *'), $appointment_date]
         );
-        $row->layout = ['col-sm-4', 'col-sm-4', 'col-sm-4'];
-
+        $row->layout = ['col-sm-6', 'col-sm-6'];
+        
         $row = $this->form->addFields(
-            [new TLabel('Data do Agendamento *'), $appointment_date]
-        );
-        $row->layout = ['col-sm-4'];
-
-        $row = $this->form->addFields(
-            [new TLabel('Observações'), $notes]
+            [new TLabel('Observação'), $notes]
         );
         $row->layout = ['col-sm-12'];
+        
+        $this->form->addActionLink('VOLTAR', new TAction(['AppointmentList', 'onReload']), 'fa:arrow-alt-circle-left')->class = 'btn btn-sm btn-default';
+        $this->form->addActionLink('LIMPAR', new TAction([$this, 'onEdit']), 'fa:eraser')->class = 'btn btn-sm btn-default';
+        $this->form->addAction('SALVAR', new TAction([$this, 'onSave']), 'fa:save')->class = 'btn btn-sm btn-primary right';
 
-        // Botões de ação
-        $btn = $this->form->addActionLink('Voltar', new TAction(['AppointmentList', 'onReload']), 'fa:arrow-alt-circle-left');
-        $btn->class = 'btn btn-sm btn-default';
-
-        $btn = $this->form->addActionLink(_t('Clear'), new TAction([$this, 'onEdit']), 'fa:eraser');
-        $btn->class = 'btn btn-sm btn-default';
-
-        $btn = $this->form->addAction('SALVAR', new TAction([$this, 'onSave']), 'fa:save');
-        $btn->class = 'btn btn-sm btn-primary right';
-
-        // Container vertical
         $container = new TVBox;
         $container->style = 'width: 100%';
         $container->add($this->form);
@@ -98,7 +99,7 @@ class AppointmentForm extends TPage
     
             TTransaction::close();
     
-            new TMessage('info', '<h5>Agendamento salvo com sucesso!</h5>', new TAction(['AppointmentList', 'onReload']));
+            new TMessage('info', '<h5>Atendimento salvo com sucesso!</h5>', new TAction(['AppointmentList', 'onReload']));
         } catch (Exception $e) {
             new TMessage('error', $e->getMessage());
             $this->form->setData($this->form->getData());
